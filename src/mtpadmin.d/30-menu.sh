826d00bce87571a1dd0c8ab6d63ad3d24fea8b26
US CHERNETSKII <9s7v84h75k@privaterelay.appleid.com>
2026-08-30 21:00:34 +1200

    Separate legacy heredoc from CLI runtime extensions

--- src/mtpadmin.d/30-menu.sh
+++ src/mtpadmin.d/30-menu.sh
@@ -1,26 +1,3 @@
-    rec=r.get(ip) or {}
-n=(rec.get('autonomous_system_number')
-   or rec.get('as_number')
-   or rec.get('asn')
-   or (rec.get('traits') or {}).get('autonomous_system_number'))
-org=(rec.get('autonomous_system_organization')
-     or rec.get('as_organization')
-     or (rec.get('traits') or {}).get('autonomous_system_organization')
-     or '')
-if n:
-    s=str(n)
-    if s.upper().startswith('AS'): asn=s.upper()
-    else: asn='AS'+s
-    print(f'  PASS  {asn}  {org}')
-else:
-    print('  WARN  Для этого IP ASN не найден')
-    print('  Ключи записи: '+', '.join(sorted(rec.keys())))
-PYASN
-  fi
-  echo
-  echo 'Источник: DB-IP Lite (CC BY 4.0), базы хранятся только на этом сервере.'
-}
-
 geo_rebuild_cmd(){
   header
   echo
