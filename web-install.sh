2c3ba4198f48ccc3f2f3b0a11dee11cb05c43ff0
US CHERNETSKII <9s7v84h75k@privaterelay.appleid.com>
2026-09-03 19:55:39 +1200

    Sync web installer to 0.12.1

--- web-install.sh
+++ web-install.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 IFS=$'\n\t'
 
-VERSION='0.12.0'
+VERSION='0.12.1'
 BASE_COMMIT='579aef84a1e58c4768357ab7ed238a8b787d4a8a'
 ROOT='https://raw.githubusercontent.com/us-chernetskii-k-g/mtpadmin'
 RELEASE_REF=${MTPADMIN_RELEASE_REF:-main}
@@ -49,7 +49,7 @@ p=Path(sys.argv[1]); s=p.read_text(encoding='utf-8')
 checks=["VERSION='0.5.0'","PORT=9199","RestrictAddressFamilies=AF_INET AF_INET6 AF_UNIX"]
 for marker in checks:
     if marker not in s: raise SystemExit('unexpected immutable web installer: '+marker)
-s=s.replace("VERSION='0.5.0'", "VERSION='0.12.0'", 1)
+s=s.replace("VERSION='0.5.0'", "VERSION='0.12.1'", 1)
 s=s.replace("PORT=9199", "WEB_PORT=9199", 1)
 s=s.replace('$PORT', '$WEB_PORT')
 s=s.replace('RestrictAddressFamilies=AF_INET AF_INET6 AF_UNIX','RestrictAddressFamilies=AF_INET AF_INET6 AF_UNIX AF_NETLINK',1)
@@ -59,7 +59,7 @@ chmod 0700 "$TMP/base-web-install.sh"
 bash -n "$TMP/base-web-install.sh"
 bash "$TMP/base-web-install.sh"
 
-info 'Включаю актуальную веб-панель MTPADMIN 0.12.0...'
+info 'Включаю актуальную веб-панель MTPADMIN 0.12.1...'
 get_update "$TMP/update-after-web.sh"
 run_update "$TMP/update-after-web.sh"
 
